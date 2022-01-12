a248f0d6df254860d436b6d94eac4f61afee7f15
Colin MacLeod <colinlmacleod1@gmail.com>
2022-01-12 12:13:25 -0500

    Added server and landing page

--- web/app/src/pages/landing/landing.js
+++ web/app/src/pages/landing/landing.js
@@ -3,19 +3,11 @@ import logo from './img/logo.svg';
 function Landing() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="grid place-items-center h-screen">
+        <img src={logo} className="w-80" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
